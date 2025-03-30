bd3e40336d076ef6630bf1ebb8c87b4869177bd8
LautaroFuente <lautaro.fuente@yahoo.com>
2025-03-30 01:46:45 -0300

    fix error in user service to start the app

--- Backend/servicesPrisma/userService.js
+++ Backend/servicesPrisma/userService.js
@@ -1,69 +1,70 @@
-import { PrismaClient } from '@prisma/client'
-import { weeklyCalendar } from './weeklyCalendarService';
+import { PrismaClient } from "@prisma/client";
+import { weeklyCalendar } from "./weeklyCalendarService.js";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function hashPassword(password) {
-    const salt = 10;
-    const hashedPassword = await bcrypt.hash(password, salt);
-    return hashedPassword;
-  }
+  const salt = 10;
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
 
 export const user = {
-    getAllUsers: async () => {
-        try {
-            return await prisma.user.findMany();    
-        } catch (error) {
-            console.log("Error al devolver todos los usuarios en el servicio", error);
-            return null;
-        }
-    },
+  getAllUsers: async () => {
+    try {
+      return await prisma.user.findMany();
+    } catch (error) {
+      console.log("Error al devolver todos los usuarios en el servicio", error);
+      return null;
+    }
+  },
 
-    addUser: async (dataBody) => {
+  addUser: async (dataBody) => {
+    try {
+      const hashedPassword = await hashPassword(dataBody.password);
 
-        try {
-            const hashedPassword = await hashPassword(dataBody.password);
+      const user = await prisma.user.create({
+        data: {
+          ...dataBody,
+          password: hashedPassword,
+        },
+      });
 
-            const user = await prisma.user.create({
-                data: {
-                    ...dataBody,
-                    password: hashedPassword, 
-                },
-            });
+      const weeklyCalendarForUser = await weeklyCalendar.addWeeklyCalendar(
+        user.id
+      );
 
-            const weeklyCalendarForUser = await weeklyCalendar.addWeeklyCalendar(user.id);
+      return user;
+    } catch (error) {
+      console.log("Error al agregar un usuario en el servicio", error);
+      return null;
+    }
+  },
 
-            return user;   
-        } catch (error) {
-            console.log("Error al agregar un usuario en el servicio", error);
-            return null;
-        }
-    },
+  getOneUser: async (email) => {
+    try {
+      return await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+    } catch (error) {
+      console.log("Error al buscar un usuario en el servicio", error);
+      return null;
+    }
+  },
 
-    getOneUser: async (email) => {
-        try { 
-            return await prisma.user.findUnique({
-                where:{
-                    email:email
-                }
-            });   
-        } catch (error) {
-            console.log("Error al buscar un usuario en el servicio", error);
-            return null;
-        }
-    },
-
-    deleteOneUser: async (id) => {
-        try {
-            return await prisma.user.delete({
-                where:{
-                    id:id
-                }
-            });   
-        } catch (error) {
-            console.log("Error al eliminar un usuario en el servicio", error);
-            return null;
-        }
-    },
-}
+  deleteOneUser: async (id) => {
+    try {
+      return await prisma.user.delete({
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      console.log("Error al eliminar un usuario en el servicio", error);
+      return null;
+    }
+  },
+};
