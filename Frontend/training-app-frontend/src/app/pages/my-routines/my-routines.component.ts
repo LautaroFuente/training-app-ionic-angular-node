2e7d9aab5bf2c661440ca39176d4a451fc77c16c
LautaroFuente <lautaro.fuente@yahoo.com>
2025-12-17 10:31:18 -0300

    update styles of app

--- Frontend/training-app-frontend/src/app/pages/my-routines/my-routines.component.ts
+++ Frontend/training-app-frontend/src/app/pages/my-routines/my-routines.component.ts
@@ -13,80 +13,84 @@ import { GlobalUserService } from 'src/app/services/global-user.service';
   templateUrl: './my-routines.component.html',
   styleUrls: ['./my-routines.component.scss'],
   standalone: true,
-  imports: [IonicModule, RoutinesListComponent]
+  imports: [IonicModule, RoutinesListComponent],
 })
-export class MyRoutinesComponent  implements OnInit, OnDestroy, AfterViewInit {
-
+export class MyRoutinesComponent implements OnInit, OnDestroy, AfterViewInit {
   routines!: Routine[];
   swiper!: Swiper;
-  
-    private unsubscribe$ = new Subject<void>();
-  
-    constructor(private router: Router, private routineService: RoutineService, private globalUser: GlobalUserService) { }
-  
-    ngOnInit() {
-      // Obtener las rutinas del backend
-      this.obtainRoutines
-    }
 
-    // Iniciar swiper
-    ngAfterViewInit() {
+  private unsubscribe$ = new Subject<void>();
 
-      this.initSwiper();
-    }
+  constructor(
+    private router: Router,
+    private routineService: RoutineService,
+    private globalUser: GlobalUserService
+  ) {}
 
-    // Metodo para iniciar y configurar el swiper
-    initSwiper() {
-      this.swiper = new Swiper('.swiper-container', {
-        loop: true,
-        slidesPerView: 3,
-        spaceBetween: 10,
-        centeredSlides: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+  ngOnInit() {
+    // Obtener las rutinas del backend
+    this.obtainRoutines();
+  }
+
+  // Iniciar swiper
+  ngAfterViewInit() {
+    this.initSwiper();
+  }
+
+  // Metodo para iniciar y configurar el swiper
+  initSwiper() {
+    this.swiper = new Swiper('.swiper-container', {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 10,
+      centeredSlides: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
         },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+        1024: {
+          slidesPerView: 2,
         },
-        breakpoints: {
-          768: {
-            slidesPerView: 1,
-          },
-          1024: {
-            slidesPerView: 2,
-          },
-        },
-      });
-    }
+      },
+    });
+  }
 
-    // Metodo para obtener las rutinas
-    private obtainRoutines(){
-      // Obtener el id del usuario por medio del servicio correspondiente
+  // Metodo para obtener las rutinas
+  private obtainRoutines() {
+    // Obtener el id del usuario por medio del servicio correspondiente
     const userId = this.globalUser.getId();
 
     // Hacer la peticion por medio del servicio correspondiente
-      this.routineService.getAllRoutinesFromOneUser(userId).pipe(takeUntil(this.unsubscribe$)).subscribe(
-        (response) =>{
+    this.routineService
+      .getAllRoutinesFromOneUser(userId)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (response) => {
           console.log('rutinas obtenidas correctamente');
           this.routines = response;
         },
-        (error) =>{
+        (error) => {
           console.log('Error al obtener las rutinas', error);
         }
       );
-    }
-  
-    // Desuscribir al destruir el componente
-    ngOnDestroy(): void {
-      this.unsubscribe$.next();
-      this.unsubscribe$.complete();
-    }
-  
-    // Metodo para volver atras
-    goBack() {
-      this.router.navigate(['/login-menu']);
-    }
+  }
 
+  // Desuscribir al destruir el componente
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  // Metodo para volver atras
+  goBack() {
+    this.router.navigate(['/login-menu']);
+  }
 }
